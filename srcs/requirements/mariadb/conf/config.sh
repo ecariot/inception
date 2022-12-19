a9f3dcce72ab8df85bfc15dd6528050845e9e5ec
emcariot <emcariot@debian.emcariot.42.fr>
2022-12-19 09:08:02 -0600

    maria db demarre

---
+++ srcs/requirements/mariadb/conf/config.sh
@@ -0,0 +1,7 @@
+service mysql start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+exec mysqld_safe
