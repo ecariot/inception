0da64c94d2b7d156f1f893e7f0f1d89e412cfda9
emcariot <emcariot@debian.emcariot.42.fr>
2023-01-02 05:49:29 -0600

    rentree

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,2 +1,12 @@
 sleep 10
-wp config create
+wp config create	
+--allow-root \
+                --dbname=$SQL_DATABASE \
+                --dbuser=$SQL_USER \
+                --dbpass=$SQL_PASSWORD \
+                --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+if [ ! -d /run/php ]; then
+    mkdir ./run/php
+fi
+/usr/sbin/php-fpm7.3 -F
