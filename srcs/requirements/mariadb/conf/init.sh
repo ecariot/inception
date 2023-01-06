0998b4b958b28440eb3882b3d2d6d8926fd41d19
emcariot <emcariot@debian.emcariot.42.fr>
2023-01-06 11:00:33 -0600

    erreru local host pas reglee

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -1,24 +1,5 @@
-# service mysql start
 
-# if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
-# then
-#     echo "ALL OK"
-
-# else
-
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-
-# mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-# mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-
-# mysql -e "FLUSH PRIVILEGES;"
-
-# mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-
-# exec mysqld_safe
+#!/bin/sh
 
 
 if [ ! -d /var/lib/mysql/wordpress ]
@@ -31,25 +12,26 @@ then
         sleep 2
     done
     echo "Creating Wordpress database..."
-    mysql -u root <<- _EOF_
-        CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-        CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-        GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-        FLUSH PRIVILEGES;
-_EOF_
+    mysql -u root << EOF
+       CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+       CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+       GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+       FLUSH PRIVILEGES;
+EOF
+
     echo "Wordpress database created!"
 
     echo "Securing the MYSQL installation..."
     mysql -u root <<- _EOF_
-        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-        DELETE FROM mysql.user WHERE User='';
-        DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-        FLUSH PRIVILEGES;
+        echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+        echo "DELETE FROM mysql.user WHERE User='';" | mysql
+        echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" | mysql
+        echo "FLUSH PRIVILEGES;" | mysql
 _EOF_
     echo "MYSQL installation secured!"
 
     echo "Stopping temporary server!"
-    mysqladmin --user=root --password=${MYSQL_ROOT_PASSWORD} shutdown
+    mysqladmin --user=root --password=$MYSQL_ROOT_PASSWORD shutdown
 
     sleep 3
 fi
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,32 +1,34 @@
 echo "HELLO"
 
-wp core download --allow-root
 sleep 2
 
-if [ -f /var/www/html/wp-config.php ]
+rm -f /var/www/wordpress/wp-config.php
+if [ ! -f /var/www/wordpress/wp-config.php ]
 then
-    echo "wp config already exists"
-    sleep 2
-else
+    cd /var/www/wordpress/
+    wp core download --force --allow-root
+    until mysqladmin --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=mariadb ping; do
+        sleep 2
+    done
 
-echo "create wp config"
+    echo "create wp config"
 
 
-wp config create	    --allow-root \
-                    --dbname=${DB_NAME} \
-                    --dbuser=${DB_USER} \
-                    --dbpass=${DB_PASSWORD} \
-                    --dbhost=${DB_HOSTdock} --path='/var/www/wordpress'
-wp core install     --url=localhost \
-                    --title="Inception" \
-                    --admin_user=${WP_ADMIN_LOGIN} \
-                    --admin_password=${WP_ADMIN_PASSWORD} \
-                    --admin_email=${WP_ADMIN_EMAIL} \
-                    --skip-email \
-                    --allow-root
+    wp config create	--allow-root \
+                        --dbname=${MYSQL_DATABASE} \
+                        --dbuser=${MYSQL_USER} \
+                        --dbpass=${MYSQL_PASSWORD} \
+                        --dbhost=mariadb
 
-echo "OK"
-wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
-mkdir ./run/php
-/usr/sbin/php-fpm7.3 -F
-fi
+    wp core install     --url=localhost \
+                        --title="Inception" \
+                        --admin_user=${WP_ADMIN_LOGIN} \
+                        --admin_password=${WP_ADMIN_PASSWORD} \
+                        --admin_email=${WP_ADMIN_EMAIL} \
+                        --skip-email \
+                        --allow-root
+
+    echo "OK"
+    wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASSWORD
+fi;
+exec "$@"
